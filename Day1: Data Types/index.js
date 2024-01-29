4a8f3207e0f011027ec319a1aac36fef6ee5f267
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-01-29 19:45:52 +0430

    Day1: Data Types

---
+++ Day1: Data Types/index.js
@@ -0,0 +1,22 @@
+ // Declare second integer, double, and String variables.
+let t;
+let u;
+let v;
+
+// Read and save an integer, double, and String to your variables.
+
+t = parseInt(readLine())
+u = parseFloat(readLine())
+v = readLine()
+
+// Print the sum of both integer variables on a new line.
+
+console.log(i + t);
+
+// Print the sum of the double variables on a new line.
+console.log((d + u).toFixed(1));
+
+// Concatenate and print the String variables on a new line
+// The 's' variable above should be printed first.
+
+console.log(s + v)
