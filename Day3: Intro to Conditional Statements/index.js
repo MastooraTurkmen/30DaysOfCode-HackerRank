36998c6c8330b0179fb27fe0c9ed7837b11b73a3
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-01-31 15:55:28 +0430

    Day 3: Intro to Conditional Statements

---
+++ Day3: Intro to Conditional Statements/index.js
@@ -0,0 +1,27 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+
+
+function main() {
+    const N = parseInt(readLine().trim(), 10);
+}
