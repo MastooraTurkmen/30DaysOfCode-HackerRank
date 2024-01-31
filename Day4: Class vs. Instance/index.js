4d056dc26feeb6db4277c8ea687195393da142f5
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-01-31 19:38:07 +0430

    Day 4: Class vs. Instance

--- Day4: Class vs. Instance/index.js
+++ Day4: Class vs. Instance/index.js
@@ -1,10 +1,29 @@
 function Person(initialAge){
     // Add some more code to run some checks on initialAge
-  this.amIOld=function(){
-   // Do some computations in here and print out the correct statement to the console
+    let age;
+    
+    if (initialAge < 0) {
+        console.log("Age is not valid, setting age to 0.")
+        age = 0;
+    } else {
+        age = initialAge
+    }
+    
+    this.amIOld = function () {
+      
+      // Do some computations in here and print out the correct statement to the console
+      
+    if (age < 13) {
+          console.log("You are young.");
+      } else if (age >= 13 && age < 18) {
+          console.log("You are a teenager.");
+      } else {
+          console.log("You are old.");
+      }
 
   };
    this.yearPasses=function(){
           // Increment the age of the person in here
+           age ++;
    };
 }
