aade4ca1899a57f88d20956b07728fa536521ba3
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-01-31 16:22:47 +0430

    Day 3: Intro to Conditional Statements

--- Day3: Intro to Conditional Statements/index.js
+++ Day3: Intro to Conditional Statements/index.js
@@ -20,8 +20,15 @@ function readLine() {
     return inputString[currentLine++];
 }
 
-
+/*
+ * Complete the challenge below.
+ */
 
 function main() {
     const N = parseInt(readLine().trim(), 10);
+        if(N % 2 !== 0 || (N >= 6 && N<= 20)) {
+        console.log("Weird")
+    } else {
+        console.log("Not Weird")
+    }
 }
