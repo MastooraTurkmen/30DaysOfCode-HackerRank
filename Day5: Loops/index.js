e80414fe6d4e754d4ca714eac45e363534c7eaa4
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-01-31 20:08:47 +0430

    Day 5: Loops

--- Day5: Loops/index.js
+++ Day5: Loops/index.js
@@ -3,3 +3,11 @@
   The Challenge Answer:
 */
 
+function main() {
+    const n = parseInt(readLine().trim(), 10);
+    
+    for (let i = 1; i <= 10; i++) {
+        const result = `${n} x ${i} = ${n * i}`
+        console.log(result)
+    }
+}
