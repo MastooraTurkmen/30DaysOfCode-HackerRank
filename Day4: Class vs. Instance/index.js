3ef500a2407f083d477e7adfeff79eecb9cfb22d
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-01-31 16:35:55 +0430

    Day 4: Class vs. Instance

---
+++ Day4: Class vs. Instance/index.js
@@ -0,0 +1,10 @@
+function Person(initialAge){
+    // Add some more code to run some checks on initialAge
+  this.amIOld=function(){
+   // Do some computations in here and print out the correct statement to the console
+
+  };
+   this.yearPasses=function(){
+          // Increment the age of the person in here
+   };
+}
