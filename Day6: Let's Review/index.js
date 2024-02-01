88f9190f8dee2dcc7ab5edbdd81f0acd22e0567d
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-02-01 22:21:21 +0430

    Day 6: Let's Review

--- Day6: Let's Review/index.js
+++ Day6: Let's Review/index.js
@@ -1,5 +1,21 @@
 function processData(input) {
     //Enter your code here
 
-    
+    const s = input.split('\n');
+
+  for (i = 1; i < s.length; i++) {
+    let splitWord = s[i].split('');
+
+    let even = '';
+    let odd = '';
+
+    for (let i = 0; i < splitWord.length; i++) {
+      if (i % 2 === 0) {
+        even = even + splitWord[i];
+      } else {
+        odd = odd + splitWord[i];
+      }
+    }
+    console.log(even + ' ' + odd);
+  }
 } 
