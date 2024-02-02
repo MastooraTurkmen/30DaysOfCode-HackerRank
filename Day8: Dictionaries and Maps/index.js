b74f52c3417902f37532f31fbd2adef34f7ad68d
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-02-02 17:02:07 +0430

    Day 8: Dictionaries and Maps

--- Day8: Dictionaries and Maps/index.js
+++ Day8: Dictionaries and Maps/index.js
@@ -1,2 +1,19 @@
 // Day 8: Dictionaries and Maps
 
+function processData(input) {
+  input = input.split('\n');
+  let phoneBook = [];
+
+  for (let i = 1; i <= parseInt(input[0]); i++) {
+    let contactArray = input[i].split(' ');
+    phoneBook[contactArray[0]] = contactArray[1];
+  }
+
+  for (let i = (parseInt(input[0]) + 1); i < input.length; i++) {
+    if (phoneBook[input[i]]) {
+      console.log(`${input[i]}=${phoneBook[input[i]]}`);
+    } else {
+      console.log("Not found");
+    }
+  }
+} 
