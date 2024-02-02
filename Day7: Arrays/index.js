c0a7d843a488c06523d92a74a4566a5d547272bf
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-02-02 15:44:58 +0430

    Day 7: Arrays

---
+++ Day7: Arrays/index.js
@@ -0,0 +1,10 @@
+
+// Challenge Answer
+
+function main() {
+    const n = parseInt(readLine().trim(), 10);
+
+    const arr = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+        
+    console.log(arr.reverse().join(' '))
+}
