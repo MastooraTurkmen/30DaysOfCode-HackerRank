8af33343ec8b3b54e18aa45c6c35bfb004dba6c5
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-02-03 20:39:12 +0430

    Day 9: Recursion 3

---
+++ Day9: Recursion 3/index.js
@@ -0,0 +1,11 @@
+/*
+ * Complete the 'factorial' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER n as parameter.
+ */
+
+function factorial(n) {
+    // Write your code here
+
+}
