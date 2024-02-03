8f8733c7a99150f631adcbdb11696f18ef1f77ee
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-02-03 21:10:42 +0430

    Day 9: Recursion 3

--- Day9: Recursion 3/index.js
+++ Day9: Recursion 3/index.js
@@ -8,6 +8,12 @@
 function factorial(n) {
     // Write your code here
 
+    if (n === 0 || n === 1) {
+        return 1;
+    } else {
+        return n * factorial(n - 1)
+    }
+
 }
 
 function main() {
