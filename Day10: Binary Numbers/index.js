ca71dd55f710a9573c1e806fdba7c1faf414b8b7
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-02-04 10:30:44 +0430

    Day 10: Binary Numbers

---
+++ Day10: Binary Numbers/index.js
@@ -0,0 +1,19 @@
+function main() {
+    const n = parseInt(readLine().trim(), 10);
+    let binaryString = n.toString(2);
+    let maxOnes = 0;
+    let currentOnes = 0;
+    
+    for (let i = 0; i < binaryString.length; i++) {
+        if(binaryString[i] === "1"){
+            currentOnes ++;
+            maxOnes = Math.max(maxOnes, currentOnes)
+        }
+        else {
+            currentOnes = 0;
+        }
+    }
+    
+    console.log(maxOnes)
+    
+}
