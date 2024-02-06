5799adf7e547d368b273ef73399051fd9098e814
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-02-06 08:29:45 +0430

    Day 11: 2D Arrays

--- Day11: 2D Arrays/index.js
+++ Day11: 2D Arrays/index.js
@@ -6,5 +6,18 @@ function main() {
         arr[i] = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp =>    parseInt(arrTemp, 10));
     }
     
-   
+    let sumArray = [];
+    for(let i = 0; i < arr.length; i++) {
+        for(let j = 0; j < arr[i].length; j++){
+            if(arr[i] && arr[i + 1] && arr[i + 2]) {
+                if(arr[i][j + 1] != undefined && arr[i][j + 2] != undefined){
+                    sumArray.push(arr[i][j] + arr[i][j + 1] + arr[i][j + 2]
+                    + arr[i + 1][j + 1] + arr[i + 2][j] + arr[i + 2][j + 1] 
+                    + arr[i + 2][j + 2])
+                }
+            }
+        }
+    } 
+    
+    console.log(Math.max.apply(null, sumArray))
 }
