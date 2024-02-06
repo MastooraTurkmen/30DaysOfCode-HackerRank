90369eff900c7c35a4ab94c898d924b08ad7483f
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-02-06 08:31:55 +0430

    Day 12: Inheritance

--- Day12: Inheritance/index.js
+++ Day12: Inheritance/index.js
@@ -1,3 +1,24 @@
 // Day 12: Inheritance 
 
 
+class Student extends Person {
+    
+    /*	
+    *   Class Constructor
+    *   
+    *   @param firstName - A string denoting the Person's first name.
+    *   @param lastName - A string denoting the Person's last name.
+    *   @param id - An integer denoting the Person's ID number.
+    *   @param scores - An array of integers denoting the Person's test scores.
+    */
+    
+    // Write your constructor here
+    
+    /*	
+    *   Method Name: calculate
+    *   @return A character denoting the grade.
+    */
+    // Write your method here
+    
+        
+}
