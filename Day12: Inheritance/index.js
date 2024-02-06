8104d42e19642ad01ac5dfe3bebdb64de958e5b8
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-02-06 08:32:45 +0430

    Day 12: Inheritance

--- Day12: Inheritance/index.js
+++ Day12: Inheritance/index.js
@@ -2,7 +2,6 @@
 
 
 class Student extends Person {
-    
     /*	
     *   Class Constructor
     *   
@@ -13,15 +12,37 @@ class Student extends Person {
     */
     
     // Write your constructor here
+
     constructor(firstName, lastName, id, scores) {
         super(firstName, lastName, id);
         this.scores = scores    
     }
+
     /*	
     *   Method Name: calculate
     *   @return A character denoting the grade.
     */
-    // Write your method here
     
+    // Write your method here
+
+    calculate(){
+        const averageScore = this.scores.reduce((sum, acc) =>
+        sum + acc, 0) / this.scores.length;
+        
+        if(averageScore >= 90 && averageScore <= 100){
+            return 'O';
+        } else if(averageScore >= 80 && averageScore < 90) {
+            return 'E';
+        } else if(averageScore >= 70 && averageScore < 80) {
+            return 'A'
+        } else if(averageScore >= 55 && averageScore < 70) {
+            return 'P'
+        }else if(averageScore >= 40 && averageScore < 55) {
+            return 'D'
+        }else {
+            return 'T'
+        }
+        
+    }
         
 }
