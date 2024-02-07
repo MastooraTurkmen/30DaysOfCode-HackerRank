67851814a53e995e4f272220d24ccf9d1a3ed17a
MastooraTurkmen <janmastoorajan@gmail.com>
2024-02-07 20:58:18 +0430

    Day 13: Abstract Classes

---
+++ Day13: Abstract Classes/index.js
@@ -0,0 +1,20 @@
+// Declare your class here.
+    /**   
+    *   Class Constructor
+    *   
+    *   @param title The book's title.
+    *   @param author The book's author.
+    *   @param price The book's price.
+    **/
+    // Write your constructor here
+    
+
+    
+    /**   
+    *   Method Name: display
+    *   
+    *   Print the title, author, and price in the specified format.
+    **/
+    // Write your method here
+    
+// End class
