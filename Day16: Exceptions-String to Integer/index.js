550c7c5c55c578b895f4523f67c77a2c2ab7edc8
MastooraTurkmen <janmastoorajan@gmail.com>
2024-02-17 20:27:51 +0430

    Day 16: Exceptions-String to Integer

--- Day16: Exceptions-String to Integer/index.js
+++ Day16: Exceptions-String to Integer/index.js
@@ -1,2 +1,11 @@
 // Day 16: Exceptions-String to Integer
 
+
+function main() {
+    const S = readLine();
+    try {
+        isNaN(Number(S)) ? error : console.log(S)
+    } catch (error) {
+        console.log("Bad String")
+    }
+}
