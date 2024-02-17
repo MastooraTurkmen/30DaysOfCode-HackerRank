73b415c96e6b15bb3b8fc37bd9274a58b0d9ec45
MastooraTurkmen <janmastoorajan@gmail.com>
2024-02-17 20:06:42 +0430

    Day 15: Linked List

--- Day15: Linked List/index.js
+++ Day15: Linked List/index.js
@@ -1,1 +1,19 @@
 // Day 15: Linked List
+
+this.insert = function (head, data) {
+    //complete this method
+    let node = new Node(data);
+    let current = head;
+
+    if (!head) {
+        return node;
+    } else {
+        while (current.next) {
+            current = current.next;
+        }
+        current.next = node;
+    }
+
+    return head;
+};
+
