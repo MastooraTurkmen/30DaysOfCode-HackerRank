f655659048741dc6ca9580c087c2951e9c33267f
MastooraTurkmen <janmastoorajan@gmail.com>
2024-02-17 20:56:06 +0430

    Day 17: More Exceptions

--- Day17: More Exceptions/index.js
+++ Day17: More Exceptions/index.js
@@ -1,3 +1,19 @@
 // Day 17: More Exceptions
 
 // Write your code here
+
+class Calculator {
+    constructor() {
+        this.power = function (n, p) {
+            try {
+                if (n >= 0 && p >= 0) {
+                    return Math.pow(n, p)
+                } else {
+                    throw 'n and p should be non-negative'
+                }
+            } catch (err) {
+                return err
+            }
+        }
+    }
+}
