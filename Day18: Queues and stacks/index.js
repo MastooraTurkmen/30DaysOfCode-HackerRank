d6b47b28412559b444796d62b7cb75b774de52ff
MastooraTurkmen <janmastoorajan@gmail.com>
2024-02-18 15:29:07 +0430

    Day18: Queues and stacks

--- Day18: Queues and stacks/index.js
+++ Day18: Queues and stacks/index.js
@@ -1,2 +1,22 @@
 // Day18: Queues and stacks
 
+function Solution() {
+    this.stack = [];
+    this.queue = [];
+
+    this.pushCharacter = function (ch) {
+        this.stack.push(ch)
+    }
+
+    this.enqueueCharacter = function (ch) {
+        this.queue.push(ch)
+    }
+
+    this.popCharacter = function () {
+        return this.stack.pop()
+    }
+
+    this.dequeueCharacter = function () {
+        return this.queue.shift();
+    }
+}
