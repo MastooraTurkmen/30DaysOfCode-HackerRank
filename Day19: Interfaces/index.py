ac059eafcce92e732d6b94d18c244d7272d1977b
MastooraTurkmen <janmastoorajan@gmail.com>
2024-02-18 16:13:19 +0430

    Day 19 Interfaces

--- Day19: Interfaces/index.py
+++ Day19: Interfaces/index.py
@@ -1,2 +1,9 @@
 # Day 19 Interfaces
 
+class Calculator(AdvancedArithmetic):
+    def divisorSum(self, n):
+        sum = 0
+        for i in range(1, n+1):
+            if  n % i == 0 :
+                sum += i
+        return sum
