e34d711db85516666b1cdfb2a3c620beb0231384
MastooraTurkmen <janmastoorajan@gmail.com>
2024-02-19 22:10:23 +0430

    Day 20: Sorting

--- Day20: Sorting/index.js
+++ Day20: Sorting/index.js
@@ -1,2 +1,27 @@
 // Day 20: Sorting
 
+
+function main() {
+    const n = parseInt(readLine().trim(), 10);
+
+    const a = readLine().replace(/\s+$/g, '').split(' ').map(aTemp => parseInt(aTemp, 10));
+
+    // Write your code here
+    let swapped;
+    let numSwaps = 0;
+    do {
+        swapped = false;
+        for (let i = 0; i < n - 1; i++) {
+            if (a[i] > a[i + 1]) {
+                let temp = a[i];
+                a[i] = a[i + 1];
+                a[i + 1] = temp;
+                swapped = true;
+                numSwaps++
+            }
+        }
+    } while (swapped);
+    console.log('Array is sorted in ' + numSwaps + ' swaps.')
+    console.log('First Element:', a[0]);
+    console.log('Last Element:', a[n - 1]);
+}
